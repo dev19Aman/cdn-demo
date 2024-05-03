21e3da1eccee14000c060e045aaca0a6523c1239
dev19Aman <amandeve19@gmail.com>
2024-05-03 18:45:22 +0530

    first commit

---
+++ test-cdn.js
@@ -0,0 +1,12 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Select the h1 tag
+    var heading = document.getElementById("dynamic-heading");
+
+    // Set the text content of the h1 tag
+    heading.textContent = "Dynamic Heading Set by JavaScript";
+
+    // Change the color of the h1 tag
+    heading.style.color = "red"; // Change to any color you desire
+});
